793045751551b69cd300ac25c325f09255be1d48
mortluna <mortluna@umich.edu>
2021-01-27 16:55:28 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e1411341176f6e75ba4c.js"
+    "url": "webpack-runtime-e1af9227c37c50b595b3.js"
   },
   {
     "url": "framework-6c3ead24af170cdcb87c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c1dbe575fb9237991cb329665e09df0e"
+    "revision": "de4f39d35947051cdf647b564004f250"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
